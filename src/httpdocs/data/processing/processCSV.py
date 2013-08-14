f6bd7b93afdc306d2739489561b1eb8b6ab7004b
fnk <ivandilernia@gmail.com>
2013-08-13 23:22:07 -0400

    Added up to 8 levels of depth to csv processor

--- src/httpdocs/data/processing/processCSV.py
+++ src/httpdocs/data/processing/processCSV.py
@@ -2,7 +2,7 @@ import csv, json, hashlib, zlib, sys, os
 
 FIRST_YEAR = None
 LAST_YEAR = None
-MAX_LEVEL = 3
+MAX_LEVEL = 0
 
 class entry:
 
@@ -40,6 +40,7 @@ class entry:
 def generateList(inputFile):
 	global FIRST_YEAR
 	global LAST_YEAR
+	global MAX_LEVEL
 	csventries = []
 	
 	# attempt opening file if inputFile is not 
@@ -65,6 +66,9 @@ def generateList(inputFile):
 		#initialize year-range
 		if not yearSet:
 			for key, value  in row.iteritems() :
+				if(key.find('LEVEL') != -1 and key != 'LEVEL'):
+					MAX_LEVEL = max(int(key[5:]), MAX_LEVEL)
+				#year case
 				if(key.isdigit()):
 					FIRST_YEAR = int(key) if FIRST_YEAR == None else min(FIRST_YEAR, int(key))
 					LAST_YEAR = int(key) if LAST_YEAR == None else max(LAST_YEAR, int(key))
