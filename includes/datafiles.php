b1d29f13322efc8a48156db3ffe23b16d1ac7104
Rob Korobkin <rob.korobkin@gmail.com>
2017-04-26 17:10:14 -0400

    Moving data to top level data folder.

--- includes/datafiles.php
+++ includes/datafiles.php
@@ -1,12 +1,12 @@
 <?php
 	foreach($dataSections as $section){
 		echo '<script id="data-'.$section.'" type="application/json">';
-		require_once $d . 'data/'.$section.'.json';
+		require_once ABSPATH . 'data/'.$section.'.json';
 		echo '</script>';
 	}
 ?>
 
 <script id="data-home" type="application/json"> 
-  <?php require_once $d . "data/home.json";?>
+  <?php require_once ABSPATH . "data/home.json";?>
 </script>
 
