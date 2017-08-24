4ae95158b146f7a5ae85150efa8b1678a7994a77
Rob Korobkin <rob.korobkin@gmail.com>
2017-08-24 11:41:17 -0400

    Refactoring Template

--- includes/container.php
+++ includes/container.php
@@ -1,20 +1,13 @@
-<div class="container" id="avb-body">
-	<div class="row-fluid" id="avb-wrap" style="width:auto;">
+<?php
+	require_once dirname(__FILE__) . '/navbar.php';
+?>
 
-
-		<?php 
-			require_once dirname(__FILE__) . '/navbar.php';
-
-		?>
-
-		<div id="avb-app">
-			<?php
-				$pages = array("opendata" => "opendata.php", "glossary" => "glossary.php");
-				// add related pages content if needed
-				if(array_key_exists($_GET["page"], $pages)) {
-					require_once plugin_dir_path(__FILE__) . $pages[$_GET["page"]];
-				}
-			?>
-		</div>
-	</div>
+<div id="avb-app">
+	<?php
+		$pages = array("opendata" => "opendata.php", "glossary" => "glossary.php");
+		// add related pages content if needed
+		if(array_key_exists($_GET["page"], $pages)) {
+			require_once plugin_dir_path(__FILE__) . $pages[$_GET["page"]];
+		}
+	?>
 </div>
--- includes/home.php
+++ includes/home.php
@@ -5,95 +5,105 @@
 
 <div id="overlay"></div>
 <div id="avb-home">
-    <div class="container">
-        <div class="hero-unit">
-            <div id="welcome-hero">
-                <h1><?php echo $selected_budget -> meta['title']; ?></h1>
+  <div class="hero-unit">
+      <div id="welcome-hero">
 
-                <div class="home-column" id="home-col1" style="display:inline-block;">
-                    <p><?php echo $selected_budget -> meta['header_html']; ?></p>
+        <!-- BUDGET TITLE -->
+        <h1><?php echo $selected_budget -> meta['title']; ?></h1>
 
 
-                    <?php if($selected_budget -> meta['include_taxes']): ?>
-                      <div style="margin-top: 25px;">
-                          <div>
-                              <?php echo $shortName; ?> residents, see where your tax dollars
-                              go:
-                          </div>
+        <!-- FIRST COLUMN -->
+        <div class="home-column" id="home-col1" style="display:inline-block;">
+            <p><?php echo $selected_budget -> meta['header_html']; ?></p>
 
-                          <div style="display:inline-block;">
-                              Your property tax last year:
-                          </div>
+            <!-- TAX BLOCK -->
+            <?php if($selected_budget -> meta['include_taxes']): ?>
+              <div style="margin-top: 25px;">
+                  <div>
+                      <?php echo $shortName; ?> residents, see where your tax dollars
+                      go:
+                  </div>
 
-                          <div style="display:inline-block;">
-                              <span class="currencyinput">$</span>
-                              <input class="currencyinput" placeholder=2000 data-section=
-                              "expenses" id="tax-input" maxlength="6" name=
-                              "tax-amount" onkeypress=
-                              'avb.home.validate(event)' type="text">
-                              <a class="link" data-section="expenses" id=
-                              "tax-input-start" style="text-decoration:underline; cursor: pointer">Start</a>
-                          </div>
-                      </div>
-                    <?php endif; ?>
-                </div>
+                  <div style="display:inline-block;">
+                      Your property tax last year:
+                  </div>
 
-                <div class="home-column" id="home-col2" style="display:inline-block; margin-left:5%;">
-                    <div>
-                        <?php echo $selected_budget -> meta['budget_questions']; ?>
-                    </div>
+                  <div style="display:inline-block;">
+                      <span class="currencyinput">$</span>
+                      <input class="currencyinput" placeholder=2000 data-section=
+                      "expenses" id="tax-input" maxlength="6" name=
+                      "tax-amount" onkeypress=
+                      'avb.home.validate(event)' type="text">
+                      <a class="link" data-section="expenses" id=
+                      "tax-input-start" style="text-decoration:underline; cursor: pointer">Start</a>
+                  </div>
+              </div>
+            <?php endif; ?>
+        </div>
 
-                    <p style="margin-top:20px;"><a class="link" href=
-                    "javascript:;" onclick="avb.home.hide(true);" style=
-                    "text-decoration:underline;">Start the tour</a> or
-                    <a class="link" href="javascript:;" onclick=
-                    "avb.home.hide();" style=
-                    "text-decoration:underline;">Dive in</a></p>
-                </div>
+
+
+        <!-- SECOND COLUMN -->
+        <div class="home-column" id="home-col2" style="display:inline-block; margin-left:5%;">
+
+            <!-- BUDGET QUESTIONS -->
+            <div>
+                <?php echo $selected_budget -> meta['budget_questions']; ?>
             </div>
 
-            <div id="home-col3">
-                <div id="home-map-svg">
-                    <div>
-                        <div style="position: relative;height: 200px;">
+            <!-- JUMP IN QUESTIONS -->
+            <p style="margin-top:20px;">
+              <a  class="link" href="javascript:;" onclick="avb.home.hide(true);"
+                  style="text-decoration:underline;">Start the tour</a> or
+
+              <a class="link" href="javascript:;" onclick="avb.home.hide();"
+                style="text-decoration:underline;">Dive in</a>
+            </p>
+        </div>
 
 
-                            <!-- REVENUES -->
-                            <?php if(in_array("revenues", $active_sections)):?>
-                              <div class="node" data-section="revenues" style="left: 0px; width: 100px;">
-                                  <div>Revenues</div>
-                                  <div class="node-bar" id="revenues-node" style="background-color: rgb(31, 119, 180);">
-                                      <div class="node-value">11</div>
-                                  </div>
-                              </div>
-                            <?php endif; ?>
+        <!-- THIRD COLUMN -->
+        <div id="home-col3">
 
+          <!-- HOME BAR GRAPH -->
+          <div id="home-map-svg">
+              <div style="position: relative;height: 200px;">
 
-                            <!-- EXPENSES -->
-                            <?php if(in_array("expenses", $active_sections)):?>
-                              <div class="node" data-section="expenses" style="left: 100px; width: 100px;">
-                                  <div>Expenses</div>
-                                  <div class="node-bar" id="expenses-node" style=" background-color: #56b356;">
-                                      <div class="node-value">11</div>
-                                  </div>
-                              </div>
-                            <?php endif; ?>
-
-
-                            <!-- ASSETS -->
-                            <?php if(in_array("assets", $active_sections)):?>
-                              <div class="node" data-section="funds" style="left: 200px; width: 100px;">
-                                  <div>Assets</div>
-                                  <div class="node-bar" id="funds-node" style=" background-color: #ff993e;">
-                                      <div class="node-value">11</div>
-                                  </div>
-                              </div>
-                            <?php endif; ?>
-
+                  <!-- REVENUES -->
+                  <?php if(in_array("revenues", $active_sections)):?>
+                    <div class="node" data-section="revenues" style="left: 0px; width: 100px;">
+                        <div>Revenues</div>
+                        <div class="node-bar" id="revenues-node" style="background-color: rgb(31, 119, 180);">
+                            <div class="node-value">11</div>
                         </div>
                     </div>
-                </div>
-            </div>
+                  <?php endif; ?>
+
+
+                  <!-- EXPENSES -->
+                  <?php if(in_array("expenses", $active_sections)):?>
+                    <div class="node" data-section="expenses" style="left: 100px; width: 100px;">
+                        <div>Expenses</div>
+                        <div class="node-bar" id="expenses-node" style=" background-color: #56b356;">
+                            <div class="node-value">11</div>
+                        </div>
+                    </div>
+                  <?php endif; ?>
+
+
+                  <!-- ASSETS -->
+                  <?php if(in_array("assets", $active_sections)):?>
+                    <div class="node" data-section="funds" style="left: 200px; width: 100px;">
+                        <div>Assets</div>
+                        <div class="node-bar" id="funds-node" style=" background-color: #ff993e;">
+                            <div class="node-value">11</div>
+                        </div>
+                    </div>
+                  <?php endif; ?>
+
+              </div>
+          </div>
         </div>
+      </div>
     </div>
 </div>
